98c7d9d6359d264c39561dff965412bdfa9bc28a
Your Name <your.email@example.com>
2026-05-08 10:59:04 +0600

    feat(v2): add Text component with themed presets

---
+++ components/ui/v2/Text.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text as RNText, TextProps as RNTextProps } from "react-native";
+import { useTheme, type ColorPalette } from "../../../utils/theme";
+import { createTextStyles, type TextStyles } from "../../../utils/typography-v2";
+
+interface TextProps extends RNTextProps {
+  preset?: keyof TextStyles;
+  color?: keyof ColorPalette;
+}
+
+export const Text = React.memo(function Text({ preset = "body", color, style, ...props }: TextProps) {
+  const { colors } = useTheme();
+  const textStyles = createTextStyles(colors);
+  const presetStyle = textStyles[preset];
+  const colorStyle = color ? { color: colors[color] } : {};
+  return <RNText style={[presetStyle, colorStyle, style]} {...props} />;
+});
